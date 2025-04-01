becfe5d5b5264891973a48d061faf670c47bd380
Tristen Dillon <tristendillon24@outlook.com>
2025-04-01 02:49:20 +0000

    Add scaling changes if on firestick

--- frontend/hooks/use-amazon-device.tsx
+++ frontend/hooks/use-amazon-device.tsx
@@ -26,6 +26,22 @@ export default function useAmazonDevice() {
       const fireTVMatch = /AFTM|AFT[BDIKMNRS]|AFTA|AFTS|AFTB|AFTT|AFTN|FireTV/i.test(ua);
       setIsFireTV(fireTVMatch);
 
+      if (silkMatch || fireTVMatch) {
+        // Apply meta viewport scaling for Amazon devices
+        const viewport = document.querySelector('meta[name="viewport"]');
+        console.log('viewport', viewport);
+        if (viewport) {
+          viewport.setAttribute('content', 'width=device-width, initial-scale=0.5, user-scalable=no');
+        } else {
+          // If viewport meta tag doesn't exist, create one
+          const newViewport = document.createElement('meta');
+          newViewport.setAttribute('name', 'viewport');
+          newViewport.setAttribute('content', 'width=device-width, initial-scale=0.5, user-scalable=no');
+
+          document.head.appendChild(newViewport);
+        }
+      }
+
       // Set overall Amazon device flag
       setIsAmazonDevice(silkMatch || fireTVMatch);
     }
