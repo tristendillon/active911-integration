d80c7fbca35f82cc4406aba97b39b0227550bf1e
Tristen Dillon <96560330+tristendillon@users.noreply.github.com>
2025-04-04 19:36:37 +0000

    linting stuff

--- frontend/components/dashboard.tsx
+++ frontend/components/dashboard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from './header';
 import NewAlertPopover from './alert-popover/new-alert-popover';
 import { GoogleMapComponent } from './google-map-component';
@@ -10,29 +10,11 @@ import useAmazonDevice from '@/hooks/use-amazon-device';
 
 export default function Dashboard() {
   const { sound, map } = useDashboard();
-  const { isFireTV, isSilk, userAgent } = useAmazonDevice();
-  useEffect(() => {
-    // Apply scaling if Amazon device detected (either by URL param or user agent)
-    if (isFireTV || isSilk || true) {
-      let viewport = document.querySelector('meta[name="viewport"]');
-      if (!viewport) {
-        viewport = document.createElement('meta');
-        viewport.setAttribute("name", "viewport");
-        document.head.appendChild(viewport);
-      }
-      console.log("CHANGING VIEWPORT FOR AMAZON DEVICE");
-      viewport.setAttribute('content', 'width=device-width, initial-scale=0.5, user-scalable=no');
-    }
-  }, [isFireTV, isSilk]);
-
-  // Original layout for other devices
+  const { isFireTV, isSilk } = useAmazonDevice();
 
   return (
     <div className="w-full h-full overflow-hidden">
       <div className={isFireTV || isSilk ? "scale-[0.5] origin-top-left w-[200%] h-[200%]": "w-full h-full"}>
-        <div>IS firetv {String(isFireTV)}</div>
-        <div>Is silk {String(isSilk)}</div>
-        <div>ua {userAgent}</div>
         <NewAlertPopover sound={sound} />
         <div className="h-full w-full flex">
           <div className="grid w-1/2 bg-secondary grid-cols-5 h-screen overflow-hidden">
