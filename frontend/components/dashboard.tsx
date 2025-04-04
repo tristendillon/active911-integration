5d905af4057566e093f4e52178c5adab52638683
Tristen Dillon <96560330+tristendillon@users.noreply.github.com>
2025-04-04 19:30:57 +0000

    fix the width maybe?

--- frontend/components/dashboard.tsx
+++ frontend/components/dashboard.tsx
@@ -28,23 +28,25 @@ export default function Dashboard() {
   // Original layout for other devices
 
   return (
-    <>
-      <div>IS firetv {String(isFireTV)}</div>
-      <div>Is silk {String(isSilk)}</div>
-      <div>ua {userAgent}</div>
-      <NewAlertPopover sound={sound} />
-      <div className="h-full w-full flex">
-        <div className="grid w-1/2 bg-secondary grid-cols-5 h-screen overflow-hidden">
-          <Sidebar isFireTV={isFireTV} />
-        </div>
-        <div className="w-full h-full block">
-          <Header />
-          <div className="w-full h-full relative">
-            <GoogleMapComponent center={map.center} zoom={map.zoom} markers={map.markers} />
+    <div className="w-full h-full overflow-hidden">
+      <div className={isFireTV || isSilk ? "scale-[0.5] origin-top-left w-[200%] h-[200%]": "w-full h-full"}>
+        <div>IS firetv {String(isFireTV)}</div>
+        <div>Is silk {String(isSilk)}</div>
+        <div>ua {userAgent}</div>
+        <NewAlertPopover sound={sound} />
+        <div className="h-full w-full flex">
+          <div className="grid w-1/2 bg-secondary grid-cols-5 h-screen overflow-hidden">
+            <Sidebar isFireTV={isFireTV} />
+          </div>
+          <div className="w-full h-full block">
+            <Header />
+            <div className="w-full h-full relative">
+              <GoogleMapComponent center={map.center} zoom={map.zoom} markers={map.markers} />
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
 );
 
 }
