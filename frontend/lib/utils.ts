636db686117ec4b5c706c058da0841c902896520
Tristen Dillon <tristendillon24@outlook.com>
2025-04-08 04:13:38 +0000

    Fix getAlertIcon function not returning the correct icon

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -5,9 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getAlertIcon(descriptor: string) {
+export function getAlertIcon(input: string) {
   let icon: string;
-
+  const descriptor = input.toLowerCase();
   // Refactor this to have more icons in the future with a different way of handling the descriptor (constants? idk tbh)
   if (descriptor.includes('fire')) {
     icon = 'fire';
